54d229de6492d4c821806b44bf7c97b32d65db30
Ideilson Cisne <ideilson@lit.ifce.edu.br>
2021-05-07 01:51:27 -0300

    more changes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
-    has_many :products
+    has_many :products #obs Colocar sempre no plural, caso seja has_one, colocar sempre no singular
+    belongs_to :user #obs: Colocar sempre no singular
 
     def total_of_products
         products.count
